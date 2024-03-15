9d0421934d9be6aaf5b2c520ca81fbe43d316fbe
SwiftScribeDev <harithapnair79@gmail.com>
2024-03-15 11:13:00 +0530

    fixed error haritha

---
+++ src/main/java/ems/example/ems/models/Signup.java
@@ -0,0 +1,52 @@
+package ems.example.ems.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "signup_table")
+public class Signup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "signup_id")
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "user_password")
+    private String password;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
